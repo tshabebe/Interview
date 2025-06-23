abb5fec5bae2dba710008a51196beca9c045c38c
teshe <teshomeabebe114@gmail.com>
2025-06-23 23:26:01 +0300

    Refactor BingoGrid to remove unnecessary interface for bingo numbers

--- src/component1/component1.tsx
+++ src/component1/component1.tsx
@@ -36,16 +36,8 @@ function InformationButton({ item, value }: { item: string; value: number }) {
   )
 }
 
-interface BingoNumbers {
-  B: number[]
-  I: number[]
-  N: number[]
-  G: number[]
-  O: number[]
-}
-
 function BingoGrid() {
-  const bingoNumbers: BingoNumbers = {
+  const bingoNumbers = {
     B: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     I: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
     N: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
