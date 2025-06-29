d7611575bd0bed2462073488d4c273edd40c8658
teshe <teshomeabebe114@gmail.com>
2025-06-29 10:34:15 +0300

    Refactor Component2 to include InformationButton and GridComponent for enhanced functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Component2 from './component2/component2'
 
 function App() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-start flex-wrap justify-center">
       <BingoApp />
       <Component2 />
     </div>
--- src/component2/component2.tsx
+++ src/component2/component2.tsx
@@ -1,3 +1,41 @@
 export default function Component2() {
-  return <div>hello second challange</div>
+  return (
+    <div className="bg-sky-950 w-sm h-fit flex flex-col gap-4 p-2 items-center">
+      <div className="flex w-full gap-2 p-2">
+        <InformationButton item="Wallet" value={206} />
+        <InformationButton item="Active Games" value={1} />
+        <InformationButton item="Stake" value={10} />
+      </div>
+      <GridComponent />
+    </div>
+  )
+}
+
+function GridComponent() {
+  const numbers = Array.from({ length: 100 }, (_, i) => i + 1)
+  return (
+    <div className="grid w-fit grid-cols-10 gap-1">
+      {numbers.map((number) => (
+        <div
+          key={number}
+          className="size-6 bg-sky-900 rounded-md flex items-center justify-center text-sm"
+        >
+          {number}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// InformationButton component - individual stat display
+interface InformationButtonProps {
+  item: string
+  value: number
+}
+function InformationButton({ item, value }: InformationButtonProps) {
+  return (
+    <button className="flex flex-col bg-sky-900 hover:bg-sky-800 px-2 py-1 rounded-2xl grow basis-11">
+      <span className="whitespace-nowrap">{item}</span> <span>{value}</span>
+    </button>
+  )
 }
